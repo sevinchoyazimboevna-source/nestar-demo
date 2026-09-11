178dc577d64c8ad65d8d971222edc779d6461ef0
Angel <sevinchoyazimboevna@gmail.com>
2026-09-12 02:09:07 +0500

    feat: intigration header and footer via layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,19 +1,18 @@
-
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
-  console.log("COMMUNITY COMPONENT - APP ROUTER");
-
-  const [title, setTitle] = useState<string>("hello");
-
+const Community: NextPage = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+  const [title, setTitle] = useState<string>("Oi");
   return (
     <div>
-      COMMUNITY  
-      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
-        Button
+      COMMUNITY{" "}
+      <button onClick={() => alert("Oi MIT")} style={{ margin: "15px" }}>
+        PressMe
       </button>
     </div>
   );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,5 +1,11 @@
-const PropertyList = () => {
-    return <div>Property List</div>;
+
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
+  return <Container>PROPERTY LIST</Container>;
 };
 
-export default PropertyList;
+export default withLayoutBasic(PropertyList);
